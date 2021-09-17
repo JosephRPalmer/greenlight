85da603f33dd2dc597e1ca944a460950b443d597
Joseph Ryan-Palmer <jrp@Josephs-MacBook-Air.local>
2021-09-17 08:22:05 +0100

    Initial code push

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="greenlight",
+    version="0.1.5",
+    author="Joseph Ryan-Palmer",
+    author_email="joseph@ryan-palmer.com",
+    description="A small application to poll for response code or text",
+    url="https://github.com/JosephRPalmer/greenlight",
+    project_urls={
+        "Bug Tracker": "https://github.com/JosephRPalmer/greenlight/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "greenlight"},
+    packages=setuptools.find_packages(where="greenlight"),
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": ["greenlight=greenlight.greenlight:main"]
+    },
+)
